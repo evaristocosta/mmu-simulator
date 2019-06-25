e0ccf83562a08f43998f7ee6550e51b70fa36e93
Lucas Costa <31525643+evaristocosta@users.noreply.github.com>
2019-06-25 09:10:43 -0300

    correcao de caminho

--- salva_resultado.sh
+++ salva_resultado.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(./mmu ../traces/bzip.trace -lru; ./mmu ../traces/gcc.trace -lru; ./mmu ../traces/sixpack.trace -lru; ./mmu ../traces/swim.trace -lru) > ../lru.txt
-(./mmu ../traces/bzip.trace -sc; ./mmu ../traces/gcc.trace -sc; ./mmu ../traces/sixpack.trace -sc; ./mmu ../traces/swim.trace -sc) > ../sc.txt 
+(./mmu traces/bzip.trace -lru; ./mmu traces/gcc.trace -lru; ./mmu traces/sixpack.trace -lru; ./mmu traces/swim.trace -lru) > ../lru.txt
+(./mmu traces/bzip.trace -sc; ./mmu traces/gcc.trace -sc; ./mmu traces/sixpack.trace -sc; ./mmu traces/swim.trace -sc) > ../sc.txt 
